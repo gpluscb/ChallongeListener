3ca6fe78a5ee0e482cf811bfb2204afe795fde1c
MarRue <20143778+gpluscb@users.noreply.github.com>
2019-10-05 23:57:42 +0200

    Commented ChallongeListenerExample

--- src/examples/java/com/gpluscb/challonge_listener/ChallongeListenerExample.java
+++ src/examples/java/com/gpluscb/challonge_listener/ChallongeListenerExample.java
@@ -2,7 +2,6 @@ package com.gpluscb.challonge_listener;
 
 import java.util.Scanner;
 
-import com.google.gson.GsonBuilder;
 import com.gpluscb.challonge_listener.events.tournament.GenericTournamentEvent;
 import com.gpluscb.challonge_listener.events.tournament.TournamentDescriptionChangedEvent;
 import com.gpluscb.challonge_listener.listener.ListenerAdapter;
@@ -13,45 +12,64 @@ import at.stefangeyer.challonge.exception.DataAccessException;
 import at.stefangeyer.challonge.model.Credentials;
 import at.stefangeyer.challonge.model.Tournament;
 import at.stefangeyer.challonge.rest.retrofit.RetrofitRestClient;
-import at.stefangeyer.challonge.serializer.Serializer;
 import at.stefangeyer.challonge.serializer.gson.GsonSerializer;
 
+// Extending from ListenerAdapter to execute own code when events fire
 public class ChallongeListenerExample extends ListenerAdapter {
 	public static void main(final String[] args) {
+		// Allocating outside of try/catch scope so we may shut it down in case
+		// of an exception
+		ListenerManager manager = null;
+		
+		// Try-with-resource for preventing resource leaks
 		try(final RetrofitRestClient client = new RetrofitRestClient(); final Scanner s = new Scanner(System.in)) {
 			System.out.println("Starting...");
 			
-			final GsonBuilder builder = new GsonBuilder();
-			builder.serializeNulls().setPrettyPrinting();
-			final Serializer serializer = new GsonSerializer(builder);
+			// Setting credentials for accessing the Challonge api
 			final Credentials credentials = new Credentials(System.getenv("ChallongeUsername"),
 					System.getenv("ChallongeToken"));
-			final ChallongeExtension challonge = new ChallongeExtension(credentials, serializer, client);
+			// Creating ChallongeExtension instance
+			final ChallongeExtension challonge = new ChallongeExtension(credentials, new GsonSerializer(), client);
 			
-			final ListenerManager manager = new ListenerManager(challonge, 5000);
+			// Creating ListenerManager instance with a cycle time of 5s
+			manager = new ListenerManager(challonge, 5000);
 			
+			// Adding new instance of ChallongeListenerExample which extends
+			// ListenerAdapter as a listener so the overridden methods get
+			// executed when events fire
 			manager.addListener(new ChallongeListenerExample(challonge));
 			
+			// Waiting until our ListenerManager is ready
 			manager.awaitRunning();
 			
 			System.out.println("Everything running. Press enter to close.");
+			// Waiting for the user to input enter
 			s.nextLine();
 			System.out.println("Stopping...");
 			
+			// Shutting down our ListenerManager so that its thread does not
+			// keep our application alive indefinitely
 			manager.shutdown();
-			client.close();
-			s.close();
 			
+			// Waiting until our ListenerManager is completely shut down
 			manager.awaitState(ManagerState.SHUT_DOWN);
 			
 			System.out.println("Stopped.");
 		} catch(final DataAccessException | IllegalStateException | InterruptedException e) {
+			// *Something* went wrong *somewhere*. Trying to shut down our
+			// manager only if it already exists.
+			if(manager != null) {
+				manager.shutdown();
+			}
+			
 			e.printStackTrace();
 		}
 	}
 	
 	public ChallongeListenerExample(final ChallongeExtension challonge) throws DataAccessException {
 		System.out.println("Subscribed to tournaments:");
+		
+		// Subscribing to all owned and co-owned tournaments
 		for(final Tournament tournament : challonge.getTournaments()) {
 			subscribeTo(tournament.getId().longValue());
 			System.out.println(tournament.getFullChallongeUrl());
@@ -61,7 +79,7 @@ public class ChallongeListenerExample extends ListenerAdapter {
 	@Override
 	public void onGenericTournamentEvent(final GenericTournamentEvent event) {
 		// Removes all time based events, as they are fired constantly
-		// for some reason.
+		// due to an issue in the api.
 		if(!event.getClass().getName().endsWith("AtChangedEvent")) {
 			System.out.println(event + " in Tournament " + event.getTournament().getFullChallongeUrl());
 		}
